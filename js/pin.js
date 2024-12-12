77e25e4ff94198b0fac2b47533d43abdd930a1ec
Lerts <oddff@mail.ru>
2024-12-12 21:23:42 +0300

    v.0.0.13
    работа над таблицей

--- js/pin.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".tableC__icon-pin").forEach(function (item) {
-  item.addEventListener("click", function (event) {
-    item.classList.toggle("tableC__icon-pin--disabled");
-  });
-});
